6b4bdd2ca0291e92ee9a83026b41dcfde7b41d81
Pranav Manu <71229090+raipranav384@users.noreply.github.com>
2023-04-21 23:00:30 +0530

    smal changes/comments

--- vol_renderer.py
+++ vol_renderer.py
@@ -203,9 +203,7 @@ def vol_render(
         t=strat_sampler(near,far,num_samples,device=device)
         # t=t[None,:,None]
     rays=rays_o[:,None,:]+rays_d[:,None,:]*t[None,:,None]
-    # print("RAYS_SHAPE:",rays[...,0].max(),rays[...,1].max(),rays[...,2].max(),rays[...,0].min(),rays[...,1].min(),rays[...,2].min())
     orig_shape=rays.shape
-    # print("t_shape:",rays_o[:,None,:].shape)
     if Pos_encode is not None and Dir_encode is not None:
         rays=rays.reshape(-1,3)
         dirs=rays_d[...,None,:].repeat(1,num_samples,1)
@@ -222,8 +220,8 @@ def vol_render(
     else:
         print("ERROR: No positional encoding")
         # return
-    # else:
-    #     print("SOMEHOW HERE!!")
+
+    # with torch.cuda.amp.autocast():
     model_out=model(rays,dirs)
     model_out=model_out.reshape(orig_shape[0],orig_shape[1],-1)
     sigma=model_out[...,3]
@@ -242,28 +240,6 @@ def vol_render(
 
     return C
 
-def cumprod_exclusive(
-  tensor: torch.Tensor
-) -> torch.Tensor:
-  r"""
-  (Courtesy of https://github.com/krrish94/nerf-pytorch)
-  Mimick functionality of tf.math.cumprod(..., exclusive=True), as it isn't available in PyTorch.
-  Args:
-  tensor (torch.Tensor): Tensor whose cumprod (cumulative product, see `torch.cumprod`) along dim=-1
-    is to be computed.
-  Returns:
-  cumprod (torch.Tensor): cumprod of Tensor along dim=-1, mimiciking the functionality of
-    tf.math.cumprod(..., exclusive=True) (see `tf.math.cumprod` for details).
-  """
-
-  # Compute regular cumprod first (this is equivalent to `tf.math.cumprod(..., exclusive=False)`).
-  cumprod = torch.cumprod(tensor, -1)
-  # "Roll" the elements along dimension 'dim' by 1 element.
-  cumprod = torch.roll(cumprod, 1, -1)
-  # Replace the first element by "1" as this is what tf.cumprod(..., exclusive=True) does.
-  cumprod[..., 0] = 1.
-  
-  return cumprod
 
 def find_bounding_box(images,poses,near,far,focal,device=None):
     if device is None:
