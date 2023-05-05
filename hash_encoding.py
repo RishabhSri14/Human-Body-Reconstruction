dd779d1f50099517443b412a0fd717a9d627cd86
Pranav Manu <71229090+raipranav384@users.noreply.github.com>
2023-05-05 13:28:31 +0530

    Faster hash encoding implemented

--- hash_encoding.py
+++ hash_encoding.py
@@ -21,23 +21,32 @@ class HashEncoder(nn.Module):
         self.mu=0 if mu is None else mu
         # self.mu=torch.tensor(self.mu).to(self.device)
         # self.sigma=torch.tensor(self.sigma).to(self.device)
-        self.pis=torch.tensor(np.array([1, 2654435761,805459861],dtype=np.int32),device=self.device)
+        pis=torch.tensor(np.array([1, 2654435761,805459861],dtype=np.int32),device=self.device)
+        self.register_buffer('pis',pis,persistent=False)
         if self.dim==2:
             self.pis=self.pis[None,:dim]
         if self.dim==3:
             self.pis=self.pis[None,None,:dim]
         for i in range(self.L):
-            Embedding_list.append(nn.Embedding(T,F,sparse=True))
+            Embedding_list.append(nn.Embedding(T,F,sparse=False))
             nn.init.uniform_(Embedding_list[-1].weight,a=-1e-4,b=1e-4)
         self.Embedding_list=(nn.ModuleList(Embedding_list))
-    
-    def hash_func(self,x:np.ndarray,T:torch.tensor):
+        num_vertices=2**self.dim
+        neigs = np.arange(num_vertices, dtype=np.int64)
+        dims = np.arange(self.dim, dtype=np.int64)
+        bin_mask = torch.tensor((neigs[:,None] & (1 << dims[None,:]))==0, dtype=bool) # (neig, dim)
+        print(bin_mask)
+        self.register_buffer('bin_mask', bin_mask, persistent=False)
+
+    @torch.no_grad()
+    def hash_func(self,x:torch.tensor,T:torch.tensor):
         # pis=torch.tensor(np.array([1, 2654435761,805459861],dtype=np.int32))
         ndims=self.dim
-        if self.dim==2:
-            prod=x*self.pis
-        if self.dim==3:
-            prod=x*self.pis
+        # if self.dim==2:
+        #     prod=x*self.pis
+        # if self.dim==3:
+        #     prod=x*self.pis
+        prod=x*self.pis[:ndims]
         val=torch.bitwise_xor(prod[...,0],prod[...,1])
         if ndims==3:
             val=torch.bitwise_xor(val,prod[...,2])
@@ -115,26 +124,46 @@ class HashEncoder(nn.Module):
 
             c=c0*(1-diff[...,1:2])+c1*diff[...,1:2]
         return c
+    def fast_get_indices(self,x:torch.tensor,x_val:torch.tensor,out:torch.tensor=None):
+
+        # if out is None:
+        #     out=torch.zeros(x.shape[0],2**self.dim,self.dim,device=self.device)
+        # idxs=torch.zeros_like(self.bin_mask,dtype=torch.long,device=self.device)
+        # out[...,self.bin_mask]=x_val[...,0:1][self.bin_mask]
+        # out[...,~(self.bin_mask)]=x_val[...,1:2][self.bin_mask]
+        # print("X_VAL_SHAPE:",x_val.shape)
+        out=torch.where(self.bin_mask,x_val[...,0],x_val[...,1])
+        return out
+    
+    def fast_nlinear_interpolation(self,x:torch.tensor,feature_vecs:torch.tensor,x_val:torch.tensor,diff:torch.tensor):
+        # wts=torch.zeros_like(feature_vecs,device=self.device)
+        # wts[...,self.bin_mask]=1-diff
+        # wts[...,~(self.bin_mask)]=diff
+        wts=torch.where(self.bin_mask,1-diff,diff)
+        wts=wts.prod(dim=-1,keepdim=True)
+        return (feature_vecs*wts).sum(dim=-2)
     
     def forward(self,x,aux=None):
         assert(x.shape[-1]==self.dim)
         y=torch.zeros(x.shape[0],self.F*self.L+self.E,device=self.device)
         out=torch.zeros(x.shape[0],2**self.dim,self.dim,device=self.device)
         # t1_sum,t2_sum,t3_sum=0,0,0
+        self.bin_mask=self.bin_mask.reshape((1,)*(len(x.shape)-1)+(2**self.dim,self.dim))
         for i in range(self.L):
             N_l=self.N_min*self.b**i
             un_x=((x-self.mu)/self.sigma)*N_l
             # print(un_x.shape,x.shape,self.mu.shape,self.sigma.shape)
             # t1=time.time()
-            idxs=self.get_indices(un_x,out=out)
-            # t1=time.time()-t1
-            # t2=time.time()
-            idx_hash=self.hash_func(idxs,T=self.T)
-            # t2=time.time()-t2
-            # t3=time.time()
-            interp_vec=self.ninear_interpolation(un_x,(self.Embedding_list[i])(idx_hash))
-            # t3=time.time()-t3
-            y[:,i*self.F:(i+1)*self.F]=interp_vec
+            bdims = len(x.shape[:-1])
+            x_val=torch.stack([un_x.long(),un_x.long()+1],dim=-1)
+            diff=(un_x-x_val[...,0])
+            x_val=x_val[...,None,:,:]
+            diff=diff[...,None,:].detach()
+            idxs=self.fast_get_indices(x,x_val,out)
+            hash_idxs=self.hash_func(idxs,self.T)
+            feature_vecs=self.Embedding_list[i](hash_idxs)
+            interp_vecs=self.fast_nlinear_interpolation(x,feature_vecs,x_val,diff)
+            y[:,i*self.F:(i+1)*self.F]=interp_vecs
             # t1_sum+=t1
             # t2_sum+=t2
             # t3_sum+=t3
