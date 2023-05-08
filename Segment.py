813267e12a31cde06100cdf8313f1b7d52871af5
Rishabh Srivastava <83019436+RishabhSri14@users.noreply.github.com>
2023-05-08 16:20:09 +0530

    Update Segment.py

--- Segment.py
+++ Segment.py
@@ -10,6 +10,20 @@ from tqdm import tqdm
 
 with open("config.yaml","r") as f:
     config=yaml.load(f,Loader=yaml.FullLoader)
+
+
+def get_bounding_box(images):
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+    model.eval()
+    boxes=np.zeros((len(images),4))
+    for i in range(len(images)):
+        output = model((transforms.ToTensor()(images[i])).unsqueeze(0))
+        box = output[0]['boxes'].detach().cpu().numpy()
+        boxes[i] = box[0]
+        
+    return boxes
+
+
 image_path =glob(config["segmentation"]["input"]+"/*")
 
 images = [Image.open(i).convert('RGB') for i in image_path]
@@ -21,17 +35,18 @@ transform_dlv3 = transforms.Compose([
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 input_tensor_dlv3=torch.stack(([transform_dlv3(i) for i in images]),dim=0)
-
+    
 
 # Load the pre-trained model from PyTorch Hub
 model = torchvision.models.segmentation.deeplabv3_resnet101(pretrained=True)
 with torch.no_grad():
     output = model(input_tensor_dlv3)['out']
     
-# print(output.shape) 
 mask = output.argmax(1).byte().cpu().numpy()
-# print(mask.shape)
 final_mask=np.zeros(input_tensor_dlv3.shape)
+
+boxes=get_bounding_box(images)
+print(boxes.shape)
 for i in range(mask.shape[0]):
     original_tensor = input_tensor_dlv3[i].cpu().numpy()
     original_tensor = original_tensor*std.reshape([3,1,1])+mean.reshape([3,1,1])
@@ -43,7 +58,11 @@ for i in range(mask.shape[0]):
 fig,axes=plt.subplots(len(images), 2, figsize=(20, 100))
 for i  in range(len(images)):
     axes[i,0].imshow(images[i])
+    axes[i,0].plot([boxes[i][0], boxes[i][2]], [boxes[i][1], boxes[i][1]], color='r', linewidth=2)
+    axes[i,0].plot([boxes[i][0], boxes[i][0]], [boxes[i][1], boxes[i][3]], color='r', linewidth=2)
+    axes[i,0].plot([boxes[i][0], boxes[i][2]], [boxes[i][3], boxes[i][3]], color='r', linewidth=2) 
+    axes[i,0].plot([boxes[i][2], boxes[i][2]], [boxes[i][1], boxes[i][3]], color='r', linewidth=2)
     axes[i,0].axis('off')
     axes[i,1].imshow((np.transpose(final_mask[i],(1,2,0))*255).astype(np.uint8),cmap='gray')
     axes[i,1].axis('off')
-plt.savefig("./SegmentationResults.png")
+plt.savefig("./SegmentedImages/SegmentationResults.png")
